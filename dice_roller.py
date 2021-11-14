d7a24763d35586c93e835974029057e06e20455a
bssayla <ouaicha47@gmail.com>
2021-11-14 01:12:43 +0100

    Now rolling two dices

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,15 @@
-import random
 def main():
+  import random
 
-  roll = random.randint(1, 6)
+  dice_rolls = 2
+  dice_sum = 0
 
-  print(f'You rolled a {roll}')
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+    dice_sum += roll
+  print(f'You rolled a total of {dice_sum}')
   
+
 if __name__== "__main__":
   main()

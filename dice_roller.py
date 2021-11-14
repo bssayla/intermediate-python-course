dc1913382ff81d05ab4bb8f85f8a2bcb5469cfae
bssayla <ouaicha47@gmail.com>
2021-11-14 01:36:42 +0100

    Small database added

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,7 @@
 def main():
   import random
+  player_name = input("What is your name? ")
+  player_score = 0
   dice_size = int(input("How many sides does your dice have? "))
   dice_rolls = int(input("How many times do you want to roll the dice? "))
   dice_sum = 0
@@ -9,11 +11,15 @@ def main():
     dice_sum += roll
     if roll == 1:
       print(f"You rolled a {roll} Critical Fail!")
+      player_score -= 1
     elif roll == dice_size:
       print(f"You rolled a {roll} Critical Success!")
+      player_score += 1
     else:
       print(f'You rolled a {roll}')
-  print(f'You rolled a total of {dice_sum}')
+  data_base = open("dice_roller_data.txt", "a")
+  data_base.write(f"{player_name} rolled {dice_rolls} times with a {dice_size} sided dice for a total of {dice_sum} points.\n")
+  print(f'You rolled a total of {dice_sum} your score is {player_score}')
   
 
 if __name__== "__main__":
